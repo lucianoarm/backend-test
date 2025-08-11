ab29fb363054b74aa57d8352f7b468f0363a5436
lucianoarm <eu@lucianomeneses.com.br>
2025-08-11 20:41:32 -0300

    Inicialização do framework symfony

---
+++ test-api/README.md
@@ -0,0 +1,20 @@
+# SCRIPT SQL para iniciar o Banco de Dados da API
+-- Remove o banco se já existir
+DROP DATABASE IF EXISTS `db_test_api`;
+
+-- Cria o banco
+CREATE DATABASE `db_test_api`
+    DEFAULT CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+-- Remove o usuário (se já existir, remove também privilégios)
+DROP USER IF EXISTS 'usuario_api'@'localhost';
+
+-- Cria o usuário novamente (host = localhost)
+CREATE USER 'usuario_api'@'localhost' IDENTIFIED BY '.!usuarioApi2025!.';
+
+-- Concede privilégios completos no banco criado
+GRANT ALL PRIVILEGES ON `db_test_api`.* TO 'usuario_api'@'localhost';
+
+-- Aplica mudanças
+FLUSH PRIVILEGES;
---
+++ test-api/migrations/Version20250811222556.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250811222556 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE investment (id INT AUTO_INCREMENT NOT NULL, owner_id INT NOT NULL, created_at DATE NOT NULL, initial_value DOUBLE PRECISION NOT NULL, redeemed_value DOUBLE PRECISION DEFAULT NULL, redeemed_at DATE DEFAULT NULL, INDEX IDX_43CA0AD67E3C61F9 (owner_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE owner (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(150) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE investment ADD CONSTRAINT FK_43CA0AD67E3C61F9 FOREIGN KEY (owner_id) REFERENCES owner (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE investment DROP FOREIGN KEY FK_43CA0AD67E3C61F9');
+        $this->addSql('DROP TABLE investment');
+        $this->addSql('DROP TABLE owner');
+    }
+}
